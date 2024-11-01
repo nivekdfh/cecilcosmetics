52c453000e99e5b8dc6f772fed8f1f8fe0028db3
nivekdfh <nivekam94@gmail.com>
2024-11-01 10:07:14 +0100

    Resolved merge conflicts and updated gh-pages

--- server.js
+++ server.js
@@ -16,11 +16,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB using environment variable
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("MongoDB connection error:", error));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
+
 
 // Define the Booking Schema and Model
 const BookingSchema = new mongoose.Schema({
