2f3089c61ae7ed50a02d66498ab6fd8e04d1473d
nivekdfh <nivekam94@gmail.com>
2024-11-01 09:28:34 +0100

    Resolved merge conflicts and updated gh-pages

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,8 +1,11 @@
-// vite.config.js
+// vite.config.jsx
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cecilcosmetics/', // Replace with your actual GitHub repository name
+  base: process.env.NODE_ENV === 'production' ? '/cecilcosmetics/' : '/',
+  build: {
+    outDir: 'dist'
+  }
 });
